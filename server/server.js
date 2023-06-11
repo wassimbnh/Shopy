04a032027db03fc2d397737d755d3736e8238e39
wassimbnh <104159727+wassimbnh@users.noreply.github.com>
2023-06-11 23:40:07 +0100

    crypto payment set up

--- server/server.js
+++ server/server.js
@@ -7,7 +7,16 @@ const paymentRoute = require('./routes/paymentRoute');
 const cookieParser = require('cookie-parser')
 const PORT = 4000;
 
+
 const app = express();
+
+var coinbase = require('coinbase-commerce-node');
+var Client = coinbase.Client;
+var ressources = coinbase.resources;
+ 
+Client.init(process.env.COINBASE_API_KEY);
+
+
 
 app.use(cors());
 app.use(express.static("public"));
@@ -24,7 +33,30 @@ mongoose.connect(
 //routes
 app.use('/api/auth',userRoutes);
 app.use( paymentRoute);
+app.post("/crypto-checkout", async (req,res)=>{
+    const { amount, currency} = req.body;
 
+    try{
+
+        const charge = await ressources.Charge.create({
+            name: 'The Sovereign Individual',
+    description: 'Mastering the Transition to the Information Age',
+    local_price: {
+        amount: amount,
+        currency: currency
+    },
+    pricing_type: 'fixed_price',
+    
+        });
+
+        res.status(200).json({
+            charge: charge,
+
+        })
+    }catch(err){
+        res.status(500).json({msg: err.message})
+    }
+});
 
 app.listen(PORT,
      () => console.log("Listening on port 4000"));
