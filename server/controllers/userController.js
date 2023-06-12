c6332b1885a1cc2dda89d2caf833047be544786b
wassimbnh <104159727+wassimbnh@users.noreply.github.com>
2023-06-12 11:55:18 +0100

    forget password

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -210,11 +210,105 @@ const userController = {
     forgetPassword: async(req,res) =>{
         try{
 
+          // get email
+      const { email } = req.body;
+
+      // check email if exits in db
+      const user = await User.findOne({ email });
+      if (!user)
+        return res.status(400).json({ msg: "This email is not registered in our system." });
+      
+      // create ac token
+      const ac_token = createToken.access({ id: user.id });
+
+        // send email
+      const url = `http://localhost:3000/auth/reset-password/${ac_token}`;
+
+      mailSender(email, "FORGOT PASSWORD TOKEN ", `<!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF8" />
+          <meta httpequiv="XUACompatible" content="IE=edge" />
+          <meta name="viewport" content="width=devicewidth, initialscale=1.0" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap"
+            rel="stylesheet"
+          />
+          <title>Passioncorners | Account Activation</title>
+          <style>
+            body {
+              background-color: #333333;
+              height: 100vh;
+              font-family: "Roboto", sans-serif;
+              color: #fff;
+              position: relative;
+              text-align: center;
+            }
+            .container {
+              max-width: 700px;
+              width: 100%;
+              height: 100%;
+              margin: 0 auto;
+            }
+            .wrapper {
+              padding: 0 15px;
+            }
+            .card {
+              position: absolute;
+              top: 50%;
+              left: 50%;
+              transform: translate(50%, 50%);
+              width: 100%;
+            }
+            span {
+              color: #008000;
+            }
+            h1 span {
+              color: yellow;
+            }
+            button {
+              padding: 1em 6em;
+              border-radius: 5px;
+              border: 0;
+              background-color: green;
+              transition: all 0.3s ease-in;
+              cursor: pointer;
+            }
+            button:hover {
+              background-color: hsl(45, 70%, 51%);
+              transition: all 0.3s ease-in;
+            }
+            .spacing {
+              margin-top: 5rem;
+            }
+          </style>
+        </head>
+        <body>
+          <div class="container">
+            <div class="wrapper">
+              <div class="card">
+                <h1><span>Welcome !</span></h1>
+                <p>Please reset your password by clicking on the button below 👇🏻</p>
+                <a href=${url}><button>Reset your password</button></a>
+                <p class="spacing">
+                  If the button above does not work, please navigate to the link
+                  provided below 
+                </p>
+              </div>
+            </div>
+          </div>
+        </body>
+      </html>
+      `);
+
+      // success
+      res.status(400).json({ msg: "Resend the password, please check your email." });
+
         }catch(err){
             res.status(400).json({msg: err.message});
         }
     },
-
+ 
     resetPassword: async(req,res) =>{
 
     },
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -6,6 +6,7 @@ route.post('/register', userController.register);
 route.post('/activation', userController.activate);
 route.post('/sign-in', userController.signin);
 route.post('/access', userController.access);
+route.post('/forgot-password', userController.forgetPassword);
 
 
 module.exports = route;
