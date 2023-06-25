bd107c56b43ab85c053d6b90810afbce517b70b2
wassimbnh <104159727+wassimbnh@users.noreply.github.com>
2023-06-25 11:40:33 +0100

    signup form validation

--- store/src/components/SignUp.js
+++ store/src/components/SignUp.js
@@ -13,9 +13,17 @@ import {
 }
 from 'mdb-react-ui-kit';
 import { CDBBtn } from "cdbreact";
+import { useForm } from 'react-hook-form'
 
 
 function SignUp() {
+
+  const form =useForm();
+  const { register, formState, watch } = form;
+  const { errors } = formState;
+
+  const password = watch('password');
+
   return (
     <MDBContainer fluid>
 
@@ -25,25 +33,64 @@ function SignUp() {
             <MDBCol md='10' lg='6' className='order-2 order-lg-1 d-flex flex-column align-items-center'>
 
               <h2 classNAme="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4 pb-3">Sign up</h2>
-
+              <form action="">
               <div className="d-flex flex-row align-items-center mb-4 ">
                 <MDBIcon fas icon="user me-3" size='lg'/>
-                <MDBInput label='Your Name' id='form1' type='text' className='w-100'/>
+                <MDBInput 
+                    label='Your Name'
+                    id='name'
+                    type='text'
+                    className='w-100'
+                    {...register("name",{
+                    require: "Name is required"
+                    })}/>
+                    <p className='error-message'>{errors.name?.message}</p>
               </div>
 
               <div className="d-flex flex-row align-items-center mb-4">
                 <MDBIcon fas icon="envelope me-3" size='lg'/>
-                <MDBInput label='Your Email' id='form2' type='email'/>
+                <MDBInput label='Your Email'
+                          id='email'
+                          type='email'
+                           className='w-100'
+                          {...register("email",{
+                          require: "Email is required",
+                          pattern: {
+                            value:  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                            message: 'Invalid email format',
+                          },
+                        })}/>
+                        <p className='error-message'>{errors.email?.message}</p>
               </div>
 
               <div className="d-flex flex-row align-items-center mb-4">
                 <MDBIcon fas icon="lock me-3" size='lg'/>
-                <MDBInput label='Password' id='form3' type='password'/>
+                <MDBInput label='Password'
+                          id='password'
+                           type='password'
+                           {...register("password",{
+                            require: "Password is required",
+                            minLength:{
+                              value:8,
+                              message: "Password should have at least 8 characters"
+                            }
+                          })}/>
+                          <p className='error-message'>{errors.password?.message}</p>
               </div>
 
               <div className="d-flex flex-row align-items-center mb-4">
                 <MDBIcon fas icon="key me-3" size='lg'/>
-                <MDBInput label='Repeat your password' id='form4' type='password'/>
+                <MDBInput
+                        label='Confirm your password'
+                        id='confirmPassword'
+                        type='password'
+                        {...register('confirmPassword', {
+                          required: 'Confirm password is required',
+                          validate: (value) => value === password || 'Passwords do not match',
+                        })}
+                        />
+                  {errors.confirmPassword &&
+                   <p className='error-message'>{errors.confirmPassword.message}</p>}
               </div>
 
               <div className='mb-4'>
@@ -52,6 +99,7 @@ function SignUp() {
 
               <CDBBtn color='primary' circle size='lg' className='px-4 py-2'>Register</CDBBtn>
 
+              </form>
             </MDBCol>
 
             <MDBCol md='10' lg='6' className='order-1 order-lg-2 d-flex align-items-center'>
