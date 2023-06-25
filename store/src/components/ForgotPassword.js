053ae0aedf88e5f7f7898d33f8d01249307fc465
wassimbnh <104159727+wassimbnh@users.noreply.github.com>
2023-06-25 16:54:54 +0100

    froget pwd form validation

--- store/src/components/ForgotPassword.js
+++ store/src/components/ForgotPassword.js
@@ -11,9 +11,20 @@ import {
 }
 from 'mdb-react-ui-kit';
 import { CDBBtn } from "cdbreact";
+import { useForm } from 'react-hook-form';
+
 
 
 function ForgotPassword() {
+
+  const form = useForm();
+const { register, formState, handleSubmit } = form;
+const { errors } = formState;
+
+const onSubmit = (data) => {
+console.log('form submit', data);
+};
+
   return (
     <MDBContainer fluid>
 
@@ -25,16 +36,31 @@ function ForgotPassword() {
               <h2 classNAme="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4 pb-3">Forgot password</h2>
 
               
+              <form onSubmit={handleSubmit(onSubmit)}>
 
               <div className="d-flex flex-row align-items-center mb-4">
                 <MDBIcon fas icon="envelope me-3" size='lg'/>
-                <MDBInput label='Your Email' id='form2' type='email'/>
-              </div>
+                <MDBInput
+                            label='Your Email'
+                            id='email'
+                            type='email'
+                            className='w-100'
+                            {...register('email', {
+                            required: 'Email is required',
+                            pattern: {
+                            value: /^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)$/,
+                            message: 'Invalid email format',
+                            },
+                            })}
+                           />  
+                   <p className='text-danger'>{errors.email?.message}</p>
+                </div>
               
               <div className='d-flex align-items-center '>
-              <CDBBtn color='primary' circle size='lg' className='px-4 py-2'>Reset</CDBBtn>
-
+              <CDBBtn color='primary' circle size='lg' className='px-4 py-2' type='submit'>Reset</CDBBtn>
+            
               </div>
+              </form>
             </MDBCol>
 
             <MDBCol md='10' lg='6' className='order-1 order-lg-2 d-flex align-items-center'>
--- store/src/components/SignIn.js
+++ store/src/components/SignIn.js
@@ -19,7 +19,7 @@ return (
     <MDBRow>
           <MDBCol md='10' lg='6' className='order-2 order-lg-1 d-flex flex-column align-items-center'>
       <h2 className='text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4 pb-3'>Sign in</h2>
-      <form action='' onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit(onSubmit)}>
       <div className='d-flex flex-row align-items-center mb-4'>
     <MDBIcon fas icon='envelope me-3' size='lg' />
       <MDBInput
