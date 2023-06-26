f9b75e8efd9289cd54636fa4b4b2a1dabf3547b2
wassimbnh <104159727+wassimbnh@users.noreply.github.com>
2023-06-26 19:54:14 +0100

    add reducer

--- store/src/redux/store.js
+++ store/src/redux/store.js
@@ -1,11 +1,12 @@
 
 import { configureStore } from '@reduxjs/toolkit';
 import signupReducer from '../redux/signupSlice';
+import activateSlice from './activateSlice';
 
 const store = configureStore({
   reducer: {
     auth: signupReducer,
-    // Add other reducers here if needed
+    activ: activateSlice
   },
 });
 
